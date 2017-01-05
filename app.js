cb4e54cbe9d034428c8dab74e7d729911bf2f6ce
battlequinn <quinnthebattle@gmail.com>
2017-01-05 14:03:37 -0600

    got Billy Bob to work

--- app.js
+++ app.js
@@ -20,45 +20,50 @@ function initMostWanted(people){
 	}while(!(SearchType == "name"|| SearchType == "attributes")); 
 	switch(SearchType){
 		case "name":
-		var person = getPerson(prompt("Enter person's first name."), prompt("Enter person's last name."), people);
-		mainMenu(person, people);
+			var listOfPeople = getPerson(prompt("Enter person's first name."), prompt("Enter person's last name."), people);
+			if(listOfPeople.length > 0){
+				mainMenu(listOfPeople[0], people);
+			} else {
+				alert("Did not find anyone matching those perameters.");
+			}
 		break;
 		case "attributes":
 
-		do{
-			var Gender = prompt("What is their gender? (M=Male, F=Female)");
-		}while(!(Gender == "F" || Gender == "M"));
+			do{
+				var Gender = prompt("What is their gender? (M=Male, F=Female)");
+			}while(!(Gender == "F" || Gender == "M"));
 
-		do{
-		var Age = prompt("Approximately how old are they? Please Type: '18 & Under', '18-25', '25-50' or '50+' ");
-	}while(!(Age == "18 & Under" || Age == "18-25" || Age == "25-50" || Age == "50+"));
+			do{
+			var Age = prompt("Approximately how old are they? Please Type: '18 & Under', '18-25', '25-50' or '50+' ");
+			}while(!(Age == "18 & Under" || Age == "18-25" || Age == "25-50" || Age == "50+"));
 
-			var Height = prompt("Approximately how tall is this person? (In INCHES)");
-			// (Inches, FT, In)
+				var Height = prompt("Approximately how tall is this person? (In INCHES)");
+				// (Inches, FT, In)
 
-			var Weight = prompt("About how much does this person weigh? (In LBS)");
+				var Weight = prompt("About how much does this person weigh? (In LBS)");
 
-		do{
-			var EyeColor = prompt("What is their eye color? Options: Brown, Blue, Hazel, Green or Black");
-		}while(!(EyeColor == "Brown" || EyeColor == "Blue" || EyeColor == "Hazel" || EyeColor == "Green" || EyeColor == "Black"));
+			do{
+				var EyeColor = prompt("What is their eye color? Options: Brown, Blue, Hazel, Green or Black");
+			}while(!(EyeColor == "Brown" || EyeColor == "Blue" || EyeColor == "Hazel" || EyeColor == "Green" || EyeColor == "Black"));
 
-			var Occupation = prompt("What is their line of work? (EX: Nurse)");
-			// Dropdown
+				var Occupation = prompt("What is their line of work? (EX: Nurse)");
+				// Dropdown
 
-			mainMenu(person, people);
-			break;
-			default:
+				mainMenu(person, people);
+		break;
+		default:
 			alert("There was an error processing your request.");
 			initMostWanted(people);
-			break;
+		break;
 		}
 	}
 
-	function getPerson (firstName, lastName, people){
-			var result = people.filter(function(person){(person.firstName === firstName) && (person.lastName === lastName)});
-			return getPersonInfo(people);	
-		}
-	// if user enterd info that matches with data, alert further info for the individual
+	function getPerson(firstName, lastName, people){
+		var listOfPeople = people.filter(function(person){
+			return (person.firstName === firstName) && (person.lastName === lastName);
+		});
+		return listOfPeople;
+	}
 
 	function mainMenu (person, people){
 		if(!person){
@@ -74,30 +79,30 @@ function initMostWanted(people){
 			case "info":
 			getPersonInfo(people);
 
-			break;
+				break;
 			case "family":
 			getFamily(person, people);
 
-			break;
+				break;
 			case "kin":
 			getNextOfKin(person, people);
 
-			break;
+				break;
 			case "descendants":
 			getDescendants(person, people);
 
-			break;
+				break;
 			case "restart":
 			initMostWanted(people);
 
-			break;
+				break;
 			case "quit":
 
-			break;
+				break;
 			default:
 			alert("There was an error processing your request.");
 			return mainMenu(person, people);
-			break;
+				break;
 		}
 	}
 
