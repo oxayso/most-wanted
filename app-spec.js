33a666ed650fdd648ad7510c7346aad4d1a50a4d
oxayso <oxayso@outlook.com>
2017-01-15 10:20:09 -0600

    added successful test getSiblings-Two

--- app-spec.js
+++ app-spec.js
@@ -60,7 +60,14 @@ describe('getNames', function(){
     });
 });
 
-
+describe('getNames-Two', function(){
+  beforeEach(function(){
+  selectedPeople = [{id:272822514,firstName:"Billy",lastName:"Bob",gender:"male",dob:"1/18/1949",height:71,weight:175,eyeColor:"brown",occupation:"programmer",parents:[],currentSpouse:401222887},{id:401222887,firstName:"Uma",lastName:"Bob",gender:"female",dob:"4/1/1947",height:65,weight:162,eyeColor:"brown",occupation:"assistant",parents:[],currentSpouse:272822514}];
+    });
+    it('twoPeople', function(){
+      expect(getNames(selectedPeople)).toEqual("Billy Bob and Uma Bob");
+    });
+});
 
 describe('getNames-None', function(){
     it('none', function(){
